b00710f5a5e0b14f481a832d8a44021f7a5c2154
AntonioSeixas1 <78570746+AntonioSeixas1@users.noreply.github.com>
2021-06-18 17:37:46 -0300

    :wrench: add inputs to UC

--- EXP9/UC_project.vhd
+++ EXP9/UC_project.vhd
@@ -1,10 +1,10 @@
-----------------------------------------------------------------
+-----------------------------------------------------------------------
 --! @file UC_project.vhd
 --! @author Antonio Lago Araújo Seixas <antonioseixas00@usp.br>
 --! @author Lucas Moreira Oliveira <lucasmoreiraoliveira@usp.br>
---! @brief Control unity for the project
+--! @brief Control unity for the project Autonumus iluminations System
 --! @date 2021-06-18
------------------------------------------------------------------
+-----------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -13,16 +13,18 @@ entity UC_project is
     port (
         Clock, Reset:               	            in std_logic;
         Sensor_presenca:                            in std_logic;
+        Confirma:                                   in std_logic;
         Fim_cont:                                   in std_logic;
-        Potencia                                    in std_logic_vector(1 downto 0);
+        Potencia:                                   in std_logic_vector(1 downto 0);
+        Enable_reg:                                 out std_logic;
         Enable_cont:                                out std_logic;
         Liga_luminaria:                             out std_logic;
-        db_state:                                    out std_logic_vector(1 downto 0)
+        db_state:                                   out std_logic_vector(1 downto 0)
         );                	            
 end entity;
 
 architecture comportamental of UC_project is
-    type Tipo_estado is (desligado_st, ligado_st, contando_st);
+    type Tipo_estado is (inicial_st, carga_st, desligado_st, ligado_st, contando_st);
     signal Eatual, Eprox: Tipo_estado;
 
     begin
@@ -31,16 +33,23 @@ architecture comportamental of UC_project is
         process( reset, clock)
         begin
             if reset='1' then
-                Eatual<=desligado_st;
+                Eatual<=inicial_st;
             elsif clock'event and clock='1' then
                 Eatual<=Eprox;
             end if;
         end process;
 
         -- proximo estado
-        process(Sensor_presenca, fim_cont, Eatual)
+        process(Sensor_presenca, Fim_cont, Confirma, Eatual)
         begin
             case Eatual is
+                when inicial_st =>          Eprox <= carga_st;
+
+                when carga_st =>            if confirma = '1'
+                                            then Eprox <= desligado_st;
+                                            else Eprox <= carga_st;
+                                            end if;
+
                 when desligado_st =>        if Sensor_presenca='1' and Potencia /= "00"
                                             then Eprox <= ligado_st;
                                             else Eprox<= desligado_st;
@@ -59,7 +68,7 @@ architecture comportamental of UC_project is
                                             end if;
       
 
-                when others =>              Eprox <= desligado_st;
+                when others =>              Eprox <= inicial_st;
                 
             end case;
         end process;
